3bc0307abad5fae3937d38232cfb981b7f50e449
Unmold Labs <unmold.labs@outlook.com>
2026-04-25 20:30:39 -0400

    add size limit

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const unmold = {
-    api: {
-        url: 'https://c5e0kl1q11.execute-api.us-east-1.amazonaws.com',
-        token: '',
-    }
-}
+  api: {
+    url: "https://c5e0kl1q11.execute-api.us-east-1.amazonaws.com",
+    uploadSizeLimitMB: 20,
+  },
+};
